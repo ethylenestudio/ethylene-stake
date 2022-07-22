f26e6d41f0cefdf7d8c71936cf20491696bec827
asgarovf <farhad@getwhelp.com>
2022-07-22 07:36:37 +0100

    fix: title

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <>
-      <Header title="Title" />
+      <Header title="Home" />
       <Navbar />
     </>
   );
